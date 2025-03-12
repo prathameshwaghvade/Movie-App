c440762b04432b1828383f0c5d0a89f26a4127ce
prathamesh <luffygithub@gmail.com>
2025-03-11 21:13:21 -0700

    created our first hook and tomorrow I will create the api key to fetch the movies info from themoviedb.org

--- src/components/MovieApp.js
+++ src/components/MovieApp.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MovieApp.css'
 import { AiOutlineSearch } from "react-icons/ai";
 
 const MovieApp = () => {
+    const {searchQuery,setsearchQuery}=useState('');
+
+    const handleSearchChange = (event) =>{
+        setsearchQuery(event,target,value);
+    }
+
+    const handleSearchSubmit = async 
   return (
     <div>
         <h1>MoviesHome</h1>
-        <div className="search-bar">
-            <input type='text' placeholder='Search Movies..' value='' onChange='' className='search-input'></input>
-            <button onClick='' className='search-button'>
+        <div className='search-bar'>
+            <input type='text' placeholder='Search Movies..' value={searchQuery} onChange={handleSearchSubmit} className='search-input'></input>
+            <button onClick={handleSearchSubmit} className='search-button'>
             <AiOutlineSearch />
             </button>
         </div>
